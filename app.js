795205552d78bbfb96c8ddad834b53c81417a4de
Jon <jon@allen8r.com>
2015-02-05 20:35:04 -0700

    finish step 4, part of which was already done when I did step 3. step 4 suggests a completely different method, but the way I did it works, so I'm just gonna go with it...

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
+    //set default variables
+    var color = 'white';
+    
+    
     $('.box').on('click', function() {
-        $(this).addClass('white');
+        $(this).addClass(color);
     });
     $('.box').on('dblclick', function() {
         $(this).removeClass('white');
@@ -9,6 +13,23 @@ $(document).ready(function() {
         $('.box').attr('class', 'box')/*.removeClass('white')*/;
     });
     
-    
-    
+    //Colors for boxes - original color variable is set at the top of this document
+     $('#red').on('click', function() {
+           color = 'red';
+     });
+     $('#green').on('click', function() {
+           color = 'green';
+     });
+     $('#blue').on('click', function() {
+           color = 'blue';
+     });
+     $('#yellow').on('click', function() {
+           color = 'yellow';
+     });
+     $('#white').on('click', function() {
+           color = 'white';
+     });
+     
+     
+     
 });
